6eea58722fd2c6adc4b6d581ec00aa8dc3739702
Madikara Shailendra <shailendrasai0910@gmail.com>
2023-10-21 12:35:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'; 
-import './App.css';
-import abhiImage from "./bharath.jpg"; 
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import abhiImage from "./img.jpg";
 
 function App() {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -20,23 +20,22 @@ function App() {
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-    handleScroll(); 
+    handleScroll();
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
-  
   const handleLinkClick = (e, target) => {
     e.preventDefault();
     const targetElement = document.querySelector(target);
     if (targetElement) {
       window.scrollTo({
         top: targetElement.offsetTop,
-        behavior: 'smooth',
+        behavior: "smooth",
       });
     }
   };
@@ -46,81 +45,136 @@ function App() {
       <header>
         <div className="user">
           <img src={abhiImage} alt="Bharath Reddy" />
-          <h3 className="name">Bharth Reddy</h3>
+          <h3 className="name">Madikara Shailendra</h3>
         </div>
         <nav className="navbar">
           <ul className="nav">
-            <li className='navlink'><a href="#home">HOME</a></li>
-            <li className='navlink'><a href="#about">ABOUT</a></li>
-            <li className='navlink'><a href="#education">EDUCATION</a></li>
-            <li className='navlink'><a href="#contact">CONTACT</a></li>
+            <li className="navlink">
+              <a href="#home">HOME</a>
+            </li>
+            <li className="navlink">
+              <a href="#about">ABOUT</a>
+            </li>
+            <li className="navlink">
+              <a href="#education">EDUCATION</a>
+            </li>
+            <li className="navlink">
+              <a href="#contact">CONTACT</a>
+            </li>
           </ul>
         </nav>
       </header>
 
-      <div id="menu" className={`fas ${isMenuOpen ? 'fa-times' : 'fa-bars'}`} onClick={handleMenuClick}></div>
+      <div
+        id="menu"
+        className={`fas ${isMenuOpen ? "fa-times" : "fa-bars"}`}
+        onClick={handleMenuClick}
+      ></div>
 
       <section className="home" id="home">
         <h3>HELLO!!</h3>
-        <h1>This is <span>Bharath Reddy</span></h1>
+        <h1>
+          I am <span>Shailendra</span>
+        </h1>
       </section>
 
       <section className="about" id="about">
-        <h1 className="heading"> <span>about</span> me </h1>
+        <h1 className="heading">
+          {" "}
+          <span>about</span> me{" "}
+        </h1>
         <div className="row">
           <div className="info">
-            <h3> <span> Name : </span> Bharath Reddy </h3>
-            <h3> <span> Qualification : </span> B.Tech(pursuing) </h3>
-            <h3> <span> Address</span> Godavari District</h3>
-            <h3> <span> Strengths : </span> Honesty, Patience </h3>
-            <h3> <span> Hobbies: </span> Playing Games, Travelling </h3>
-            <h3> <span> Language : </span> Telugu, English, Hindi </h3>
+            <h3>
+              {" "}
+              <span> Name : </span> Madiakara Shailendra{" "}
+            </h3>
+            <h3>
+              {" "}
+              <span> Qualification : </span> B.Tech (Pursuing){" "}
+            </h3>
+            <h3>
+              {" "}
+              <span> Address</span> Guntur, Andhra Pradesh
+            </h3>
+            <h3>
+              {" "}
+              <span> Strengths : </span> Quick learner, Curious to learn{" "}
+            </h3>
+            <h3>
+              {" "}
+              <span> Hobbies: </span> Listening music, Learning new skills &
+              concepts{" "}
+            </h3>
+            <h3>
+              {" "}
+              <span> Language : </span> Telugu, English, Hindi{" "}
+            </h3>
           </div>
         </div>
       </section>
 
       <section className="education" id="education">
-        <h1 className="heading"> Education <span>Details</span> </h1>
+        <h1 className="heading">
+          {" "}
+          Education <span>Details</span>{" "}
+        </h1>
         <div className="box-container">
           <div className="box">
             <i className="fas fa-graduation-cap"></i>
             <span>2018-2019</span>
             <h3>SSC</h3>
-            <p style={{ fontSize: '20px' }}>Bhashyam School</p>
+            <p style={{ fontSize: "20px" }}>Ravindra Bharthi School</p>
           </div>
           <div className="box">
             <i className="fas fa-graduation-cap"></i>
             <span>2019-2021</span>
             <h3>Intermediate</h3>
             <h3>MPC</h3>
-            <p style={{ fontSize: '20px' }}>Tirumala Junior College</p>
+            <p style={{ fontSize: "20px" }}>Narayana Junior College</p>
           </div>
           <div className="box">
             <i className="fas fa-graduation-cap"></i>
             <span>2021-2025</span>
-            <h3>B.Tech</h3>
+            <h3>B.Tech (Pursuing)</h3>
             <h4> </h4>
-            <p style={{ fontSize: '20px' }}>VIT AP University</p>
+            <p style={{ fontSize: "20px" }}>VIT AP University</p>
           </div>
         </div>
       </section>
 
       <section className="contact" id="contact">
-        <h1 className="heading"> <span>contact</span> me </h1>
+        <h1 className="heading">
+          {" "}
+          <span>contact</span> me{" "}
+        </h1>
         <div className="row">
           <div className="content">
             <h3 className="title">contact info</h3>
             <div className="info">
-              <h3> <i className="fas fa-envelope"></i> bharath291003@gmail.com</h3>
-              <h3> <i className="fas fa-phone"></i> 7337223633 </h3>
-              <h3> <i className="fas fa-map-marker-alt"></i> Godavari District </h3>
+              <h3>
+                {" "}
+                <i className="fas fa-envelope"></i> shailendrasai0910@gmail.com
+              </h3>
+              <h3>
+                {" "}
+                <i className="fas fa-phone"></i> 9713543116{" "}
+              </h3>
+              <h3>
+                {" "}
+                <i className="fas fa-map-marker-alt"></i> Guntur, Andhra Pradesh{" "}
+              </h3>
             </div>
           </div>
         </div>
       </section>
 
       {showTopButton && (
-        <a href="#home" className="top" onClick={(e) => handleLinkClick(e, '#home')}>
+        <a
+          href="#home"
+          className="top"
+          onClick={(e) => handleLinkClick(e, "#home")}
+        >
           <img src={abhiImage} alt="" />
         </a>
       )}
